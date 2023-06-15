d2fd8a18acc545fb16e1520878e25ddc11195649
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-06-15 21:21:37 +0900

    📝fix : 버그 수정

--- src/apis/@core.js
+++ src/apis/@core.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import API_CONST from 'consts/api'
 
 const axiosInstance = axios.create({
-	baseURL: API_CONST.BACKEND_URL + API_CONST.OWNER_REPO_TYPE,
+	baseURL: process.env.REACT_APP_BACKEND_URL + API_CONST.OWNER_REPO_TYPE,
 	headers: {
-		Authorization: 'Bearer' + API_CONST.TOKEN,
+		Authorization: 'Bearer' + process.env.REACT_APP_GIT_TOKEN,
 	},
 })
 
