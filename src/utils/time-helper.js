78b216fd3e89eaac63ce1a13ad50063e5f5cf3f9
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-06-16 22:22:18 +0900

    🌈feat : date formmat change

--- src/utils/time-helper.js
+++ src/utils/time-helper.js
@@ -1,7 +1,30 @@
-import moment from 'moment/moment'
+/**
+ *
+ * @param {type Date} date
+ */
+const timeHelper = date => {
+	let result = ''
+	const thisTime = new Date()
+	const createTime = new Date(date)
+	if (thisTime.getFullYear() !== createTime.getFullYear())
+		return (result = `${
+			thisTime.getFullYear() - createTime.getFullYear()
+		} years ago`)
+	if (thisTime.getMonth() !== createTime.getMonth())
+		return (result = `${
+			thisTime.getMonth() - createTime.getMonth()
+		} months ago`)
+	if (thisTime.getDate() !== createTime.getDate())
+		return (result = `${thisTime.getDate() - createTime.getDate()} weeks ago`)
+	if (thisTime.getHours() !== createTime.getHours())
+		return (result = `${thisTime.getHours() - createTime.getHours()} days ago`)
+	if (thisTime.getMinutes() !== createTime.getMinutes())
+		return (result = `${
+			thisTime.getMinutes() - createTime.getMinutes()
+		} hours ago`)
+	result = `just now`
 
-const timeFormatter = time => {
-	return moment(time).format('YYYY-MM-DD HH:mm')
+	return result
 }
 
-export default timeFormatter
+export default timeHelper
